5b9f53f0f05470f1ca802a213b0d4edf84ea42ef
Guille <guille0596@gmail.com>
2021-04-13 09:50:11 +0200

    Audio format conversion with its own method

--- models/fileProcessing.py
+++ models/fileProcessing.py
@@ -14,26 +14,31 @@ class FileProcessing:
         self.path: str = FileProcessing.BASE_PATH + path
         self.file_type: LiteralConstants.FileType = file_type
 
-    def read_file(self) -> Optional[Union[str, List, Tuple, Dict]]:
+    def read_file(self) -> Optional[Union[str, List, Tuple, Dict, bytes]]:
         try:
-            with open(self.path, 'r') as __file:
-                if self.file_type == LiteralConstants.FileType.REG:
-                    return __file.read()
-                elif self.file_type == LiteralConstants.FileType.JSON:
-                    return json.load(__file)
-        except Exception:
+            file = open(self.path, 'r') if self.file_type != LiteralConstants.FileType.BYTES else open(self.path, 'rb')
+            if self.file_type == LiteralConstants.FileType.REG or self.file_type == LiteralConstants.FileType.BYTES:
+                return file.read()
+            elif self.file_type == LiteralConstants.FileType.JSON:
+                return json.load(file)
+        except EnvironmentError:
             LiteralConstants.STA_LOG.logger.exception(LiteralConstants.ExceptionMessages.FILE_CANT_OPEN, exc_info=True)
             return None
+        else:
+            file.close()
 
     def write_file(self, data: Union[str, List, Tuple, Dict]) -> bool:
         try:
-            with open(self.path, 'w') as __file:
-                if self.file_type == LiteralConstants.FileType.REG:
-                    __file.write(data)
-                    return True
-                elif self.file_type == LiteralConstants.FileType.JSON:
-                    json.dump(data, __file)
-                    return True
-        except Exception:
+            file = open(self.path, 'w') if self.file_type != LiteralConstants.FileType.BYTES else open(self.path, 'wb')
+            if self.file_type == LiteralConstants.FileType.REG or self.file_type == LiteralConstants.FileType.BYTES:
+                file.write(data)
+                return True
+            elif self.file_type == LiteralConstants.FileType.JSON:
+                json.dump(data, file)
+                return True
+        except EnvironmentError:
             LiteralConstants.STA_LOG.logger.exception(LiteralConstants.ExceptionMessages.FILE_CANT_WRITE, exc_info=True)
             return False
+        else:
+            file.close()
+
